443f6354375e237b0162a70797dfad3cfce68233
jasiugruszka <32547744+jasiugruszka@users.noreply.github.com>
2017-11-05 13:27:36 +0100

    Update Pileczka.cpp

--- Pileczka.cpp
+++ Pileczka.cpp
@@ -5,17 +5,17 @@ switch(wp)
     RECT r;
     GetClientRect(kwnd, &r);
     
-    if(x+vx+dx > (r.right - r.left) || x +vx < 0)
+    if(x+vx+dx > (r.right - r.left) || x +vx < 0)   //sprawdzenie czy sie nie odbija od ściany pionowej
     {
     vx= -vx;
     }
     
-    if(y+vy+dx > (r.bottom -r.top) || y+vy < 0)
+    if(y+vy+dx > (r.bottom -r.top) || y+vy < 0)    //sprawdzenie czy sie nie odbija od ściany poziomej
     {
     vy= -vy;
     }
     
-    x+= vx;
+    x+= vx; //aktualizacja prędkości piłeczki
     y+= vy;
     
     Invalidate Rect (hwnd, NULL, TRUE);
