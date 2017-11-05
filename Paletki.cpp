6ed38d243f81e400c7d7fc4cd234daa5530edfe2
jasiugruszka <32547744+jasiugruszka@users.noreply.github.com>
2017-11-05 13:37:25 +0100

    Update and rename Paletka.cpp to Paletki.cpp

---
+++ Paletki.cpp
@@ -0,0 +1,16 @@
+void DrawLeftPaddle(HDC ptr_hdc)
+{
+	int LPBottom = LPTop + 80;
+	static int x = 100;
+	RECT rctB = {20,LPTop,40,LPBottom};
+	FillRect(ptr_hdc, &rctB, hbrush);
+}
+
+
+  void DrawRightPaddle(HDC ptr_hdc)
+{
+	int RPBottom = RPTop + 80;
+	static int x = 100;
+	RECT rctB = {950,RPTop,970,RPBottom};
+	FillRect(ptr_hdc, &rctB, hBrush);
+}
